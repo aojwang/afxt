7ef66c58e01c4bd993dedb8f22536b14cca62896
grant-aa <grant@appannie.com>
2016-10-22 14:05:59 +0800

    make up ma works

--- strategy/up_ma.py
+++ strategy/up_ma.py
@@ -10,7 +10,7 @@ from utils.stock_day import stock_latest_day
 
 
 class UpMA(object):
-    def __init__(self, end_day=None, push_days=10, up_days=5, volume_increase=4):
+    def __init__(self, end_day=None, push_days=7, up_days=3, volume_increase=2):
         self.runner = SqlRunner()
         self.end_day = end_day if end_day else stock_latest_day(self.runner)
         self.push_days = push_days
@@ -22,20 +22,31 @@ class UpMA(object):
             DELETE FROM {name} WHERE update_time = now()::DATE and reason = '{reason}';
             INSERT INTO {name}
             WITH ma_close AS (
-                select distinct code
+                select distinct s.code
                 from (
-                       select code,
-                       sum(volume) filter (where p_change < 0) as cons,
-                       sum(volume) filter (where p_change >= 0) as pos
-                       from {stock_daily_more}
-                       where close >= ma5 and close >= ma10 and close >= ma20 and
-                       close >= ma30 and close >= ma60 and
-                       date >= %(end_day)s - %(push_days)s
-                       group by code
-                       having count(*) >= %(up_days)s
-                       order by code
-                       ) s
-                where pos >= %(volume_increase)s * cons
+                        SELECT code, max_close
+                        FROM (
+                           select code,
+                           sum(volume) filter (where p_change < 0) as cons,
+                           sum(volume) filter (where p_change >= 0) as pos,
+                           max(close) as max_close,
+                           min(close) as min_close
+                           from {stock_daily_more}
+                           where
+                           date >= %(end_day)s - %(push_days)s
+                           group by code
+                           having count(*) >= %(up_days)s
+                           order by code
+                           ) t
+                           WHERE t.pos >= %(volume_increase)s * t.cons AND
+                                 (t.max_close - t.min_close) / t.min_close >= 0.04
+                       ) s,
+                       {stock_daily_more} t
+                       WHERE s.code = t.code AND
+                             t.date = %(end_day)s AND
+                             t.close >= ma5 AND t.close >= ma10 AND t.close >= ma20 AND
+                             t.close >= ma30 AND t.close >= ma60 AND t.close >= s.max_close AND
+                             t.p_change <= 5.0 AND (t.high - t.close) / t.open <= 0.01
                 )
             SELECT DISTINCT c.code, i.name, i.industry, i.esp, i.pe, 0.0,
                    now()::DATE as update_time, '{reason}' as reason
